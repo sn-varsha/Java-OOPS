a42f60d5718165bd87daff6cbfe5d617f20c97f3
sn-varsha <varshasn1231@gmail.com>
2025-03-05 08:23:28 +0530

    Abstarct-class,Abstarct-method

---
+++ src/oopsabstract/Abstract.java
@@ -0,0 +1,30 @@
+package oopsabstract;
+
+abstract class Car{
+	public abstract void drive();
+	public abstract void fly();
+	public void playMusic() {
+		System.out.println("Play Music..");
+	}
+}
+
+abstract class WagonR extends Car{
+	public void drive(){
+		System.out.println("Driving..");
+	}
+}
+
+class UpdatedWagonR extends WagonR{
+	public void fly() {
+		System.out.println("Flying..");
+	}
+}
+
+public class Abstract{
+	public static void main(String a[]) {
+		Car obj=new UpdatedWagonR();
+		obj.drive();
+		obj.fly();
+		obj.playMusic();
+	}
+}

86dc67a49831f926f321d822080cebf90a372082
sn-varsha <varshasn1231@gmail.com>
2025-03-07 12:34:28 +0530

    Exception Handling using try catch

---
+++ src/oops/Exception/ExceptionHandlingUsingTryCatch.java
@@ -0,0 +1,21 @@
+package oops.Exception;
+
+public class ExceptionHandlingUsingTryCatch {
+
+	public static void main(String[] args) {
+		int i=0;
+		int j=0;
+		
+		try {
+			j=18/i;
+		}
+		catch(Exception e) {
+			System.out.println("Something went wrong");
+		}
+		
+		System.out.println(j);
+		System.out.println("Bye");
+
+	}
+
+}

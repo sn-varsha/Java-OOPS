9f85219c98774ea8ba3cc331426fced2c3734069
sn-varsha <varshasn1231@gmail.com>
2025-03-09 10:57:30 +0530

    try with resources

---
+++ src/oops/Exception/TryWithResources.java
@@ -0,0 +1,30 @@
+package oops.Exception;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TryWithResources {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int num =0;
+		
+//		BufferedReader br = null;
+//		try {
+//			InputStreamReader in = new InputStreamReader(System.in);
+//			br = new BufferedReader(in);
+//			num = Integer.parseInt(br.readLine());
+//			System.out.println(num);
+//		}
+//		finally {
+//			br.close();
+//		}
+		
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			num = Integer.parseInt(br.readLine());
+			System.out.println(num);
+		}
+
+	}
+
+}
